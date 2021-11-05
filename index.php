9320ca2b76d08506713821c0f2d2f1564b0f39ee
Agaba Andrew <agabaandre@gmail.com>
2021-11-05 12:53:22 +0300

    cos

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
- header('Access-Control-Allow-Origin: *');
- header("Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
- header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
- $method = $_SERVER['REQUEST_METHOD'];
- if($method == "OPTIONS") {
- die();
- }
+//  header('Access-Control-Allow-Origin: *');
+//  header("Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+//  header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+//  $method = $_SERVER['REQUEST_METHOD'];
+//  if($method == "OPTIONS") {
+//  die();
+ //}
 /**
  * CodeIgniter
  *
