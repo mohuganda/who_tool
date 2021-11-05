0d343e2031d524dbfe8345736ee525efd2d59ab7
Agaba Andrew <agabaandre@gmail.com>
2021-11-05 12:32:05 +0300

    cos

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -10,7 +10,12 @@ Class Data extends REST_Controller
     {
         
         header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == "OPTIONS") {
+        die();
+        }
         parent::__construct();
         $this->load->model('Data_Model', 'dataHandler');
        
