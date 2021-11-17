a3ce7ec754db671b6135526cbca7fb713c08613e
Agaba Andrew <agabaandre@gmail.com>
2021-11-17 13:56:26 +0300

    Fixed data model

--- application/models/Data_Model.php
+++ application/models/Data_Model.php
@@ -47,7 +47,7 @@ CLass Data_Model extends CI_Model
     public function create($data,$post) 
     {
         $insert2=$this->db->replace('records_json', $data);
-        $insert=$this->db->replace('records', $post);
+        $insert=$this->db->replace('records', "$post");
         if($insert){
         $message= array("message"=>"Saved","status"=>'1');
         }
