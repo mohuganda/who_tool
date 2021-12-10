57b89ae11fcf68badff715803fa6ec35791f7c49
Agaba Andrew <agabaandre@gmail.com>
2021-12-10 23:53:17 +0300

    T-cg

--- application/modules/data/views/data.php
+++ application/modules/data/views/data.php
@@ -1,26 +1,17 @@
-<!-- Main content -->
-
-<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <style>
     img{
         width:80px;
         border-radius:3px;
     }
 </style>
-<div id="app">
-<section class="content">
-     <div class="container-fluid">
-       <!-- Main row -->
-
-       <div class="row">
-         
-         <section class="col-lg-12 ">
-  
-
-         <table id="" class="table table-striped table-bordered nowrap mytable" style="width:100%">
+<section class="col-lg-12 connectedSortable">
+                <div class="row">
+                    <div class="col-lg-12">
+           
+    <table id="example" class="table table-striped table-bordered nowrap mytable" style="width:100%">
         <thead>
             <tr>
+            <th>#</th>
             <th>Enroller ID</th>
             <th>District</th>
             <th>Worker Type</th>
@@ -39,7 +30,8 @@
             <th>ID Expiry </th>
             <th>National ID Number</th>
             <th>National ID Card Number</th>
-            <th>Consent to save and share Information </th>
+            <th>Allow Consent</th>
+            <th>Signature</th>
             <th>Mobile Number </th>
             <th>Other Contact </th>
             <th>Mobile Money Registration Status </th>
@@ -47,87 +39,64 @@
             <th>If No, Registered Name </th>
             <th>Allow Mobile Money </th>
             <th>KYC verification </th>
-            <th>Record Date </th>
+            <th>Sync Date </th>
             </tr>
         </thead>
         <tbody>
-   
+        <?php 
+             $i=1;
+             $datas = Modules::run('data/data2');
+             foreach($datas as $dt): 
+             $staff=json_decode($dt->data); 
+             
+             ?>
            
 
-            <tr v-for='collection in collections'>
-            <td>{{JSON.parse(collection.data).user_id}}</td>
-            <td>{{JSON.parse(collection.data).district}}</td>
-            <td><span v-if="JSON.parse(collection.data).hw_type == 'chw'"> Community Health Worker </span> <span v-else> Ministry Health Worker</span></td>
-            <td><span v-if="JSON.parse(collection.data).person_photo" ><img v-bind:src="'data:image/png;base64,'+ JSON.parse(collection.data).person_photo"></span></td>
+            <tr>
+            <td><?php echo $i++; ?></td>
+            <td> <?php echo $staff->user_id; ?></td>
+            <td> <?php echo $staff->district; ?></td>
+            <td><?php  if ($staff->hw_type=='chw') { echo "Community Health worker"; } else { echo "Ministry Health worker"; }  ?></td>
+            <td><?php if(!empty(@$staff->person_photo)){?><img src="data:image/png;base64,<?php  echo @$staff->person_photo; ?> " alt="Img" /><?php } ?>
+            </td>
+            <td>
 
-            <td>{{JSON.parse(collection.data).surname}}</td>
-            <td>{{JSON.parse(collection.data).firstname}}</td>
-            <td>{{JSON.parse(collection.data).othername}}</td>
-            <td>{{JSON.parse(collection.data).birth_date}}</td>
-            <td>{{JSON.parse(collection.data).birth_place}}</td>
-            <td>{{JSON.parse(collection.data).gender}}</td>
-            <td>{{JSON.parse(collection.data).job}}</td>
-            <td>{{JSON.parse(collection.data).facility}}</td>
-            <td><span v-if="JSON.parse(collection.data).id_photo"><img v-bind:src="'data:image/png;base64,'+ JSON.parse(collection.data).id_photo"></span></td>
-            <td>{{JSON.parse(collection.data).id_type}}</td>
-            <td>{{JSON.parse(collection.data).id_number}}</td>
-            <td>{{JSON.parse(collection.data).id_expiry}}</td>
-            <td>{{JSON.parse(collection.data).national_id}}</td>
-            <td>{{JSON.parse(collection.data).national_id_card_number}}</td>
-            <td>{{JSON.parse(collection.data).consent}}</td>
-            <td>{{JSON.parse(collection.data).primary_mobile_number}}</td>
-            <td>{{JSON.parse(collection.data).other_contact}}</td>
-            <td>{{JSON.parse(collection.data).is_mm_registered}}</td>
-            <td>{{JSON.parse(collection.data).is_registered_by_hw}}</td>
-            <td>{{JSON.parse(collection.data).registered_mm_name}}</td>
-            <td>{{JSON.parse(collection.data).diff_names_consent}}</td>
-            <td>{{JSON.parse(collection.data).kyc_verification}}</td>
-            <td>{{collection.sync_date}}</td>
-           
+            <?php echo $staff->surname; ?>
+            </td>
+            <td><?php echo $staff->firstname ?></td>
+            <td><?php echo $staff->othername ?></td>
+            <td><?php echo $staff->birth_date ?></td>
+            <td><?php echo $staff->birth_place ?></td>
+            <td><?php echo $staff->gender ?></td>
+            <td><?php echo $staff->job ?></td>
+            <td><?php echo $staff->facility ?></td>
+            <td><?php if(!empty(@$staff->id_photo)){ ?> <img src="data:image/png;base64,<?php echo @$staff->id_photo; ?> " alt="Img"  /><?php }?>
+            </td>
+            <td><?php echo $staff->id_type ?></td>
+            <td><?php echo $staff->id_number ?></td>
+            <td><?php echo $staff->id_expiry ?></td>
+            <td><?php echo $staff->national_id ?></td>
+            <td><?php echo $staff->national_id_card_number ?></td>
+            <td><?php echo $staff->consent ?></td>
+            <td><?php echo $staff->primary_mobile_number ?></td>
+            <td><?php echo $staff->other_contact ?></td>
+            <td><?php echo $staff->is_mm_registered ?></td>
+            <td><?php echo $staff->is_registered_by_hw ?></td>
+            <td><?php echo $staff->registered_mm_name ?></td>
+            <td><?php if(!empty(@$staff->signature)){ ?> <img src="data:image/png;base64,<?php echo @$staff->signature; ?> " alt="Img"  /><?php }?>
+            </td>
+            <td><?php echo $staff->diff_names_consent ?></td>
+            <td><?php echo $staff->kyc_verification ?></td>
+            
+            <td><?php echo $dt->sync_date ?></td>
            
             
             </tr>
-          
+            <?php endforeach; ?>
         </tbody>
 </table>
-        
-        
-         
-         
-         </section>
-       </div>
-       <!-- /.row (main row) -->
-     </div><!-- /.container-fluid -->
-   </section>
-   <!-- /.content -->
- </div>
- <script>
-  var app = new Vue({
-  el: '#app',
-  data: {
-    collections: "",
 
-  },
-  mounted: function () {
-    
-    this.records()
-    
-  },
-  methods: {
-    records: function(){
-
-      axios.get('<?php echo base_url() ?>data/collections')
-      .then(function (response) {
-          app.collections = response.data;
-         console.log(response.data);
-      })
-      .catch(function (error) {
-         console.log(error);
-      });
-    },
-    
-   
-    }
-  }
-)
-</script>
+        <!-- /.box -->
+    </div>
+</div>
+</section>                           
