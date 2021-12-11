3fc84cb0fc20a5c672fb12037087d0a9d3d7cf73
Agaba Andrew <agabaandre@gmail.com>
2021-12-11 16:15:01 +0300

    ignore

--- application/modules/data/views/data.php
+++ application/modules/data/views/data.php
@@ -111,7 +111,7 @@
              foreach($files as $dt): 
              $staff=json_decode($dt->data); 
 
-             print_r($dt);
+             print_r(json_decode($dt));
              
              ?>
            
