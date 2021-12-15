bebfd50c0838983dbf02d38d1c983fba9d08a71b
Agaba Andrew <agabaandre@gmail.com>
2021-12-16 00:37:39 +0300

    dashboard

--- application/modules/data/views/data.php
+++ application/modules/data/views/data.php
@@ -61,7 +61,7 @@ $facilities=Modules::run("auth/getFacilities");
                   
                         
     
-                <div class="form-group col-md-2">
+                <div class="form-group col-md-3">
             
                 <button type="submit" class="btn bt-sm bg-gray-dark color-pale"  style="width:100px;"><i class="fa fa-tasks" aria-hidden="true"></i>Apply</button>
                
