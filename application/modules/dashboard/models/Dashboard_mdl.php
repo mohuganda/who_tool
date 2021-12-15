cf1f19af0fa48cc64da886567987a010845f4071
Agaba Andrew <agabaandre@gmail.com>
2021-12-16 00:15:47 +0300

    dashboard

--- application/modules/dashboard/models/Dashboard_mdl.php
+++ application/modules/dashboard/models/Dashboard_mdl.php
@@ -16,10 +16,10 @@ class Dashboard_mdl extends CI_Model {
         $data['daily_updates']=$this->db->query("SELECT count(id) as daily_updates from records_json where DATE_FORMAT(sync_date, '%Y-%m-%d')='$today'")->row()->daily_updates;
         $data['total_enrollers']=$this->db->query("SELECT count(user_id) as users from user where user_id!=1")->row()->users;
         $data['active_enrollers']= $this->db->query("SELECT count(distinct(user_id)) as active_enrollers from records_json where DATE_FORMAT(sync_date, '%Y-%m-%d')='$today' and user_id!=1")->row()->active_enrollers.' out of ';
-        // $data['chwdata']= $this->db->query("SELECT JSON_EXTRACT(data,'$.hw_type') as community_workers from records_json WHERE JSON_EXTRACT(data,'$.hw_type')='chw'")->num_rows();
-        // $data['mhwdata']= $this->db->query("SELECT JSON_EXTRACT(data,'$.hw_type') as ministry_workers from records_json WHERE JSON_EXTRACT(data,'$.hw_type')='mhw'")->num_rows();
-      
-    return $data;
+        //  $data['chwdata']= $this->db->query("SELECT JSON_EXTRACT(data,'$.hw_type') as community_workers from records_json WHERE JSON_EXTRACT(data,'$.hw_type')='chw'")->num_rows();
+        //  $data['mhwdata']= $this->db->query("SELECT JSON_EXTRACT(data,'$.hw_type') as ministry_workers from records_json WHERE JSON_EXTRACT(data,'$.hw_type')='mhw'")->num_rows();
+      print_r($data);
+    //return ;
     //
 
     }
