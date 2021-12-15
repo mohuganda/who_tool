75710114142520966a0c5478596ec11c482146dc
Agaba Andrew <agabaandre@gmail.com>
2021-12-15 22:54:16 +0300

    T-cg

--- application/modules/dashboard/models/Dashboard_mdl.php
+++ application/modules/dashboard/models/Dashboard_mdl.php
@@ -14,7 +14,7 @@ class Dashboard_mdl extends CI_Model {
         $data['total_records']=$this->db->query("select count(id) as total_records from records_json")->row()->total_records;
         $today=date('Y-m-d');
         $data['daily_updates']=$this->db->query("select count(id) as daily_updates from records_json where DATE_FORMAT(sync_date, '%Y-%m-%d')='$today'")->row()->daily_updates;
-        $data['total_enrollers']=$this->db->query("select count(user_id) as users from records_json where user_id!=1)='$today'")->row()->users;
+        $data['total_enrollers']=$this->db->query("select count(user_id) as users from records_json where user_id!=1")->row()->users;
         $data['active_enrollers']=$this->db->query("select count(distinct(user_id)) as active_enrollers from records_json where DATE_FORMAT(sync_date, '%Y-%m-%d')='$today' and user_id!=1")->row()->active_enrollers;
       
     return $data;
