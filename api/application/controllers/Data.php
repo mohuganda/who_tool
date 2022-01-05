f260b8fa1f74eb231ad3caa0c7a5d8a96a700610
Agaba Andrew <agabaandre@gmail.com>
2022-01-05 22:00:35 +0300

    dashboard

--- api/application/controllers/Data.php
+++ api/application/controllers/Data.php
@@ -92,8 +92,25 @@ Class Data extends REST_Controller
             @$ihris_pid=json_decode($post)->ihris_pid;
         }
         else{
-            @$ihris_pid=json_decode($post)->ihris_pid; 
+            @$ihris_pid=""; 
         }
+        //national id
+        if(isset(json_decode($post)->national_id)){
+            @$nin=json_decode($post)->national_id;
+        }
+        else{
+            @$nin=""; 
+        }
+        //card number
+        if(isset(json_decode($post)->national_id_card_number)){
+            @$nin_cardno=json_decode($post)->national_id_card_number;
+        }
+        else{
+            @$nin_cardno=""; 
+        }
+        //id expiry
+        
+        
         $app_version = json_decode($post)->app_version;
         if(($user_id==200) ||
          ($user_id==2115)){
@@ -102,45 +119,45 @@ Class Data extends REST_Controller
         else{
         $insert2=array('app_version'=>$app_version,'user_id'=>$user_id,'reference'=>$data_reference,'data'=>$post,'district'=>$district,'hw_type'=>$hwtype);
         $insert=array(
-                    'surname'=>json_decode($post)->surname,
+                    'surname'=>@json_decode($post)->surname,
                     'ihris_pid'=>$ihris_pid,
-                    'hw_type'=>json_decode($post)->hw_type,
-                    'firstname'=>json_decode($post)->firstname,
-                    'othername'=>json_decode($post)->othername,
-                    'birth_date'=>json_decode($post)->birth_date,
-                    'birth_place'=>json_decode($post)->birth_place,
-                    'gender'=>json_decode($post)->gender,
-                    'job'=>json_decode($post)->job,
-                    'facility'=>json_decode($post)->facility,
-                    'id_type'=>json_decode($post)->id_type,
-                    'national_id'=>json_decode($post)->national_id,
-                    'id_expiry'=>json_decode($post)->id_expiry,
-                    'id_photo'=>json_decode($post)->id_photo,
-                    'person_photo'=>json_decode($post)->person_photo,
-                    'primary_mobile_operator'=>json_decode($post)->primary_mobile_operator,
-                    'primary_mobile_number'=>json_decode($post)->primary_mobile_number,
-                    'is_mm_registered'=>json_decode($post)->is_mm_registered,
-                    'is_registered_by_hw'=>json_decode($post)->is_registered_by_hw,
-                    'email'=>json_decode($post)->email,
-                    'registered_mm_name'=>json_decode($post)->registered_mm_name,
-                    'diff_names_consent'=>json_decode($post)->diff_names_consent,
-                    'verification_process'=>json_decode($post)->verification_process,
-                    'kyc_verification'=>json_decode($post)->kyc_verification,
-                    'other_contact'=>json_decode($post)->other_contact,
-                    'consent'=>json_decode($post)->consent,
-                    'registered_before'=>json_decode($post)->registered_before,
-                    'no_mobile_money_point'=>json_decode($post)->no_mobile_money_point,
-                    'national_id_card_number'=>json_decode($post)->national_id_card_number,
-                    'reference'=>json_decode($post)->reference,
-                    'user_id'=>json_decode($post)->firstname,
+                    'hw_type'=>@json_decode($post)->hw_type,
+                    'firstname'=>@json_decode($post)->firstname,
+                    'othername'=>@json_decode($post)->othername,
+                    'birth_date'=>@json_decode($post)->birth_date,
+                    'birth_place'=>@json_decode($post)->birth_place,
+                    'gender'=>@json_decode($post)->gender,
+                    'job'=>@json_decode($post)->job,
+                    'facility'=>@json_decode($post)->facility,
+                    'id_type'=>@json_decode($post)->id_type,
+                    'national_id'=>$nin,
+                    'id_expiry'=>@json_decode($post)->id_expiry,
+                    'kjsjksjks'=>@json_decode($post)->id_photo,
+                    'person_photo'=>@json_decode($post)->person_photo,
+                    'primary_mobile_operator'=>@json_decode($post)->primary_mobile_operator,
+                    'primary_mobile_number'=>@json_decode($post)->primary_mobile_number,
+                    'is_mm_registered'=>@json_decode($post)->is_mm_registered,
+                    'is_registered_by_hw'=>@json_decode($post)->is_registered_by_hw,
+                    'email'=>@json_decode($post)->email,
+                    'registered_mm_name'=>@json_decode($post)->registered_mm_name,
+                    'diff_names_consent'=>@json_decode($post)->diff_names_consent,
+                    'verification_process'=>@json_decode($post)->verification_process,
+                    'kyc_verification'=>@json_decode($post)->kyc_verification,
+                    'other_contact'=>@json_decode($post)->other_contact,
+                    'consent'=>@json_decode($post)->consent,
+                    'registered_before'=>@json_decode($post)->registered_before,
+                    'no_mobile_money_point'=>@json_decode($post)->no_mobile_money_point,
+                    'national_id_card_number'=>$nin_cardno,
+                    'reference'=>@json_decode($post)->reference,
+                    'user_id'=>@json_decode($post)->firstname,
                     'location'=>$location,
                     'record_date'=>$recorddate,
-                    'ID_Number'=>json_decode($post)->ID_Number,
-                    'consent_image'=>json_decode($post)->consent_image,
-                    'employment_terms'=>json_decode($post)->employment_terms,
+                    'ID_Number'=>@json_decode($post)->ID_Number,
+                    'consent_image'=>@json_decode($post)->consent_image,
+                    'employment_terms'=>@json_decode($post)->employment_terms,
                     'sync_date'=>date('Y-m-d H:i:s'),
-                    'app_version'=>json_decode($post)->app_version,
-                    'district'=>json_decode($post)->district);
+                    'app_version'=>@json_decode($post)->app_version,
+                    'district'=>@json_decode($post)->district);
         $response = $this->dataHandler->create($insert,$insert2);
         }
     $this->response($response,REST_Controller::HTTP_OK); 
