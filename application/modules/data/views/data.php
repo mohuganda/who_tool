a7ae35af193cedbb086eb31c05c004fe31d31c11
Agaba Andrew <agabaandre@gmail.com>
2022-03-22 20:42:31 +0300

    registration

--- application/modules/data/views/data.php
+++ application/modules/data/views/data.php
@@ -37,7 +37,7 @@ $facilities=Modules::run("auth/getFacilities");
                     <div class="form-group col-md-4">
                        <label for="aw_description">
                         Facilities </label>
-                    <select  name="facility"  class="sfacility form-control select2dist" required>
+                    <select  name="facility"  class="sfacility form-control" required>
                     <option value="" disabled>All</option>
     
                     </select>
