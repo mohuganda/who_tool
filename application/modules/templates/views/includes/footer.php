b9891a480a76b5a6e4e9c2838b91dca29d72c169
Agaba Andrew <agabaandre@gmail.com>
2022-03-22 20:25:06 +0300

    registration

--- application/modules/templates/views/includes/footer.php
+++ application/modules/templates/views/includes/footer.php
@@ -409,7 +409,7 @@ function getFacs(val) {
            url: "<?php echo base_url(); ?>auth/get_facilities",
            data:'dist_data='+val,
            success: function(data){
-              console.log(data);
+              console.log(val);
                //$(".sfacility").html(data);
            }
    });
