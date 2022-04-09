b9a3f00744997c2cb7cd99f2b622b8412bba56df
Agaba Andrew <agabaandre@gmail.com>
2022-04-09 14:31:13 +0300

    Phase2 report done

--- application/modules/dashboard/models/Dashboard_mdl.php
+++ application/modules/dashboard/models/Dashboard_mdl.php
@@ -23,7 +23,7 @@ class Dashboard_mdl extends CI_Model {
         $today=date('Y-m-d');
         $data['daily_updates']=$this->db->query("SELECT count(id) as daily_updates from records_json where DATE_FORMAT(sync_date, '%Y-%m-%d')='$today'")->row()->daily_updates;
         $data['total_enrollers']=$this->db->query("SELECT count(user_id) as users from user where user_id!=1")->row()->users;
-        $data['phase2_data']= $this->db->query("SELECT count(distinct(user_id)) as phase2_data from records_json where DATE_FORMAT(sync_date, '%Y-%m-%d')>'2022-03-31' and user_id!=1")->row()->phase2_data;
+        $data['phase2_data']= $this->db->query("SELECT count(user_id) as phase2_data from records_json where DATE_FORMAT(sync_date, '%Y-%m-%d')>'2022-03-31'")->row()->phase2_data;
  
        // $data['active_enrollers']= $this->db->query("SELECT count(distinct(user_id)) as active_enrollers from records_json where DATE_FORMAT(sync_date, '%Y-%m-%d')='$today' and user_id!=1")->row()->active_enrollers.' out of ';
         $data['chwdata']= $this->db->query("SELECT reference as community_workers from records_json WHERE hw_type='chw'")->num_rows();
