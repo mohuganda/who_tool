c4fd82bff6336640b698bdb90f36e480964e67a4
Agaba Andrew <agabaandre@gmail.com>
2022-04-10 16:17:58 +0300

    Phase2 report done

--- application/modules/data/views/pdfdata.php
+++ application/modules/data/views/pdfdata.php
@@ -70,6 +70,7 @@ img{
             <th>Mobile Money Registration Status </th>
             <th>Is registered by Health Worker </th>
             <th>If No, Registered Name </th>
+            <th>Allow Mobile Money </th>
             <th>KYC verification </th>
           
             </tr>
