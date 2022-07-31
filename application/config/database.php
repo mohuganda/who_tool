95f4c244e2b5a1d714784e89d940fc311f33de75
Agaba Andrew <agabaandre@gmail.com>
2022-07-31 19:56:40 +0300

    Ignore config

--- application/config/database.php
+++ application/config/database.php
@@ -7,10 +7,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	// 'hostname' => '172.27.1.222',
+	'hostname' => '172.27.1.222',
 	//'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'username' => 'manageatt',
+	'password' => 'manage123',
 	'database' => 'ihris_update',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
