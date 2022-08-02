9ca8f86787c6860f08663a5a2fc717ebdb36240e
Agaba Andrew <agabaandre@gmail.com>
2022-08-02 14:02:31 +0300

    Swiss reports

--- application/modules/data/models/Data_model.php
+++ application/modules/data/models/Data_model.php
@@ -25,7 +25,7 @@ class Data_model extends CI_Model
 
 			$limit = "LIMIT $starts,$limits";
 		}
-		$query = $this->db->query("SELECT * FROM `records_json_report`  WHERE status='clean' $dfilter $ffilter ORDER BY sync_date DESC $limit");
+		$query = $this->db->query("SELECT * FROM `records_json`  WHERE status='clean' $dfilter $ffilter ORDER BY sync_date DESC $limit");
 		return $query->result();
 	}
 	public function getColums()
