c3a4294a174f1ae440d2d08b9c90160266f7fc4e
Agaba Andrew <agabaandre@gmail.com>
2022-08-09 13:28:58 +0300

    upload changes

--- application/modules/data/views/clean_data.php
+++ application/modules/data/views/clean_data.php
@@ -46,7 +46,7 @@ $facilities = Modules::run("auth/getFacilities");
                         <div class="form-group col-md-4">
                             <label for="aw_description">
                                 Worker Type </label>
-                            <select name="worker_type" class="sfacility form-control">
+                            <select name="worker_type" class="form-control">
                                 <option value="" disabled>All</option>
                                 <option value="mhw">Main Stream</option>
                                 <option value="chw">VHT</option>
