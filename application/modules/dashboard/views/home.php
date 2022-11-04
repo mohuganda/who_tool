27990dfc65b8dce8a39cad3c9e62edd03054aa54
AGABA ANDREW <agabaandre@gmail.com>
2022-11-04 17:19:36 +0300

    a

--- application/modules/dashboard/views/home.php
+++ application/modules/dashboard/views/home.php
@@ -599,7 +599,7 @@
            $('#airtel_verified').text(data.airtel_verified);
            $('#others_verified').text(data.others_verified);
            mnodataGraph(data);
-           exit();
+
 
 
          }
