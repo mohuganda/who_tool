b6af5be7a2df014d2b8cbbc3f8f1613d8acf260c
AGABA ANDREW <agabaandre@gmail.com>
2022-11-04 22:22:59 +0300

    a

--- application/modules/dashboard/views/home.php
+++ application/modules/dashboard/views/home.php
@@ -113,11 +113,10 @@
              <div class="tab-content p-0">
                <!-- Morris chart - Sales -->
                <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
+                 <img id="enrolloader" src="<?php echo base_url() ?>assets/images/loader.gif" />
                  <div id="enrollment" height="300" style="height: 300px;"></div>
                </div>
-               <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
-                 <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
-               </div>
+
              </div>
            </div><!-- /.card-body -->
          </div>
