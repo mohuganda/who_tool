344d794480a5355ab15304b3dae68faef358bcab
AGABA ANDREW <agabaandre@gmail.com>
2022-11-04 19:21:17 +0300

    a

--- application/modules/dashboard/views/home.php
+++ application/modules/dashboard/views/home.php
@@ -430,28 +430,7 @@
      },
      series: [{
        name: 'Population',
-       data: [
-         ['Tokyo', 37.33],
-         ['Delhi', 31.18],
-         ['Shanghai', 27.79],
-         ['Sao Paulo', 22.23],
-         ['Mexico City', 21.91],
-         ['Dhaka', 21.74],
-         ['Cairo', 21.32],
-         ['Beijing', 20.89],
-         ['Mumbai', 20.67],
-         ['Osaka', 19.11],
-         ['Karachi', 16.45],
-         ['Chongqing', 16.38],
-         ['Istanbul', 15.41],
-         ['Buenos Aires', 15.25],
-         ['Kolkata', 14.974],
-         ['Kinshasa', 14.970],
-         ['Lagos', 14.86],
-         ['Manila', 14.16],
-         ['Tianjin', 13.79],
-         ['Guangzhou', 13.64]
-       ],
+       data: getenrollment_Data(),
        dataLabels: {
          enabled: true,
          rotation: -90,
@@ -575,7 +554,7 @@
 
    });
 
-   $(document).ready(function() {
+   function getenrollment_Data() {
      //  renderGraph(data);
 
 
@@ -585,12 +564,14 @@
        dataType: "json",
        data: '',
        success: function(data) {
-         console.log(data);
+         // console.log(data);
+
+         return data
 
 
        }
 
      });
 
-   });
+   }
  </script>
