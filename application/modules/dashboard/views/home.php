f5f43b0691b7268f7755d7ca2860d7394438b43c
AGABA ANDREW <agabaandre@gmail.com>
2022-11-06 21:51:51 +0300

    a

--- application/modules/dashboard/views/home.php
+++ application/modules/dashboard/views/home.php
@@ -240,34 +240,29 @@
            </div><!-- /.card-header -->
            <div class="card-body">
 
+             <div id="app">
+               <table id="vuedata_table" class="table table-striped table-bordered nowrap" style="width:100%">
+                 <thead>
+                   <tr>
+                     <th>#</th>
+                     <th>District</th>
+                     <th>Community HW</th>
 
-             <table class="table table-responive" id="mytable" style="width:100%">
-               <thead>
-                 <tr>
-                   <th>No</th>
-                   <th>District</th>
-                   <th>Records</th>
-                   <!-- <th>Target</th>
-                       <th>Status</th> -->
+                   </tr>
+                 </thead>
+                 <tbody>
 
-                 </tr>
-               </thead>
-               <tbody>
-                 <tr>
-                   <?php
-                    $records = Modules::run("dashboard/kyc_verified_table");
-                    $i = 1;
-                    foreach ($records as $row) : ?>
+                   <tr v-for='district in districts'>
+                     <td>{{1++}}</td>
+                     <td>{{district.district}}</td>
+                     <td>{{district.datas}}</td>
 
 
-                     <td label="No"><?php echo $i++ ?></td>
-                     <td label="District"><?php echo $row->district ?></td>
-                     <td label="Records"> <?php echo ucwords($row->datas); ?> </td>
+                   </tr>
 
-                 </tr>
-               <?php endforeach; ?>
-               </tbody>
-             </table>
+                 </tbody>
+               </table>
+             </div>
 
 
            </div>
@@ -285,3 +280,63 @@
  <script src="<?php echo base_url() ?>assets/plugins/moment/moment.min.js"></script>
  <script src="<?php echo base_url() ?>assets/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
  <?php include('chartsjs.php'); ?>
+ <script>
+   //get data for districts
+   var app = new Vue({
+     el: '#app',
+     data: {
+       districts: "",
+
+     },
+     mounted: function() {
+
+       this.dists()
+
+     },
+     methods: {
+       dists: function() {
+
+         axios.get('<?php echo base_url() ?>dashboard/kyc_verified_table')
+           .then(function(response) {
+             app.districts = response.data;
+             //console.log(response.data);
+             setTimeout(() => {
+               $('#vuedata_table').DataTable(
+
+                 {
+                   dom: 'Bfrtip',
+                   "paging": true,
+                   "lengthChange": true,
+                   "searching": true,
+                   "ordering": true,
+                   "info": true,
+                   "autoWidth": false,
+                   "responsive": true,
+                   lengthMenu: [
+                     [25, 50, 100, 150, -1],
+                     ['25', '50', '100', '150', '200', 'Show all']
+                   ],
+
+                   buttons: [
+                     'copyHtml5',
+                     'excelHtml5',
+                     'csvHtml5',
+                     'pageLength',
+
+
+                   ]
+                 }
+
+               );
+             }, 4000);
+
+           })
+           .catch(function(error) {
+             console.log(error);
+           });
+       },
+
+
+     }
+   })
+ </script>
