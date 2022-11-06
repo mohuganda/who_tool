b6825104f6b600cc9641696de631dc9febc508cb
AGABA ANDREW <agabaandre@gmail.com>
2022-11-06 21:55:02 +0300

    a

--- application/modules/dashboard/views/home.php
+++ application/modules/dashboard/views/home.php
@@ -241,7 +241,7 @@
            <div class="card-body">
 
              <div id="app">
-               <table id="vuedata_table" class="table table-striped table-bordered nowrap" style="width:100%">
+               <table id="vuedata_table" class="table" style="width:100%">
                  <thead>
                    <tr>
                      <th>#</th>
