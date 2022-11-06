436f3b2f23cf26ee8c3f7f2a2b792c338dbb3950
AGABA ANDREW <agabaandre@gmail.com>
2022-11-06 22:32:34 +0300

    a

--- application/modules/dashboard/views/chartsjs.php
+++ application/modules/dashboard/views/chartsjs.php
@@ -381,7 +381,7 @@
         axios.get('<?php echo base_url() ?>dashboard/kyc_verified_table')
           .then(function(response) {
             app.districts = response.data;
-            console.log(response.data);
+           // console.log(response.data);
             setTimeout(() => {
               $('#vuedata_table').DataTable(
 
